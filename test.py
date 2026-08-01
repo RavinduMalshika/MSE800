5cabfbf42b0a33b0902ce7cf1a70d27c1a337cce
RavinduMalshika <ravindu.malshika@gmail.com>
2026-08-02 09:16:21 +1200

    Initial commit

---
+++ test.py
@@ -0,0 +1,15 @@
+n = int(input("Enter how many number in sequence: "))
+a = 0
+b = 1
+
+for i in range(n):
+    print(a, end=" ")
+    temp = a
+    a = b
+    b = temp + b
+
+factorial = 1
+for i in range(1, n +1):
+    factorial = i * factorial
+
+print("\nFactorial: ", factorial)
