230c7269a97f8f32b97abc6dc73df19a8d563046
RavinduMalshika <ravindu.malshika@gmail.com>
2026-08-02 17:56:03 +1200

    Added Exercise 1 and 2

---
+++ Week1_Exercise1/main.py
@@ -0,0 +1,15 @@
+print("Calculate sum and product of two numbers")
+
+# take user input and store in variables of type float
+number1 = float(input("Please enter number 1 value: "))
+number2 = float(input("Please enter number 2 value: "))
+
+# find the sum of the 2 numbers
+sum = number1 + number2
+
+# find the product of the 2 numbers
+product = number1 * number2
+
+#display the results
+print("The sum of the numbers is", sum)
+print("The product of the numbers is", product)
---
+++ Week1_Exercise2/main.py
@@ -0,0 +1,11 @@
+print("BMI Calculator")
+
+# take user input for weight and height
+weight = float(input("Please enter your weight(kg): "))
+height = float(input("Please enter your height(cm): "))
+
+# calculate the BMI of user
+bmi = weight / (height ** 2)
+
+#display the results
+print(f"Your BMI is {bmi}")
