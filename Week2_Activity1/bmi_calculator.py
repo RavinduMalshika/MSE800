7d6e0bfef4fef33fb16eb9d71d360c0a8080fbb1
RavinduMalshika <ravindu.malshika@gmail.com>
2026-08-08 09:20:36 +1200

    Add Week 2 Activity 1

---
+++ Week2_Activity1/bmi_calculator.py
@@ -0,0 +1,10 @@
+class BmiCalculator:
+    pass
+
+    def calculateBmi(weight, height):
+        bmi = weight / (height ** 2)
+        return bmi
+
+BmiCalculator.weight = float(input("Please enter your weight(kg): "))
+BmiCalculator.height = float(input("Please enter your height(cm): "))
+print(f"Your BMI is {BmiCalculator.calculateBmi(BmiCalculator.weight, BmiCalculator.height)}")
