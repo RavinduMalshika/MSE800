307032c7614f043d7b61fb5b444c6919b84898fc
RavinduMalshika <ravindu.malshika@gmail.com>
2026-08-10 11:52:21 +1200

    Added converter class for OOP

--- Week2_Activity5/main.py
+++ Week2_Activity5/main.py
@@ -1,21 +1,25 @@
-def convertCelsiusToFahrenheit(value):
-    convertedValue = (value * 1.8) + 32 #celsius to fahrenheit conversion
-    return convertedValue
+class TemperatureConverter:
+    def __init__(self, value):
+        self.value = value
 
-def convertFahrenheitToCelsius(value):
-    convertedValue = (value - 32) / 1.8 #fahrenheit to celsius conversion
-    return convertedValue
+    def convertCelsiusToFahrenheit(self, value):
+        convertedValue = (value * 1.8) + 32 #celsius to fahrenheit conversion
+        print(f"{self.value} degrees Celsius is converted to {convertedValue:.2f} degrees Fahrenheit\n")
 
-def continueOperation():
-    while (True):
-        isEnd = input("Do you want to convert another value? (y/n): ").upper()
-        if (isEnd == "Y"):
-            return True
-        elif (isEnd == "N"):
-            print("-"*40 + "\n")
-            return False
-        else:
-            print("Invalid input. Enter 'y' or 'n'.")
+    def convertFahrenheitToCelsius(self, value):
+        convertedValue = (value - 32) / 1.8 #fahrenheit to celsius conversion
+        print(f"{self.value.upper()} degrees Fahrenheit is converted to {convertedValue:.2f} degrees Celsius\n")
+
+    def continueOperation(self):
+        while (True):
+            isEnd = input("Do you want to convert another value? (y/n): ").upper()
+            if (isEnd == "Y"):
+                return True
+            elif (isEnd == "N"):
+                print("-"*40 + "\n")
+                return False
+            else:
+                print("Invalid input. Enter 'y' or 'n'.")
 
 def main():
     isRunning = True
@@ -28,9 +32,9 @@ def main():
         if (userValue[0] == "C"):
             try:
                 float(userValue[1:]) #check if the rest of input valid
-                convertedValue = convertCelsiusToFahrenheit(float(userValue[1:]))
-                print(f"{userValue.upper()} degrees Celsius is converted to {convertedValue:.2f} degrees Fahrenheit\n")
-                if (continueOperation() == False):
+                converter = TemperatureConverter(userValue)
+                converter.convertCelsiusToFahrenheit(float(converter.value[1:]))
+                if (converter.continueOperation() == False):
                     isRunning = False
             except:
                 print("Invalid input. Please enter the temperature with the correct 'C' or 'F' prefix. eg: C32")
@@ -38,9 +42,9 @@ def main():
         elif (userValue[0] == "F"):
             try:
                 float(userValue[1:]) #check if the rest of input valid
-                convertedValue = convertFahrenheitToCelsius(float(userValue[1:]))
-                print(f"{userValue.upper()} degrees Fahrenheit is converted to {convertedValue:.2f} degrees Celsius\n")
-                if (continueOperation() == False):
+                converter = TemperatureConverter(userValue)
+                converter.convertFahrenheitToCelsius(float(converter.value[1:]))
+                if (converter.continueOperation() == False):
                     isRunning = False
             except:
                 print("Invalid input. Please enter the temperature with the correct 'C' or 'F' prefix. eg: C32")        
