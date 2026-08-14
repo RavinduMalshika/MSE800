681b65e97c51b5bc7b41cf9e6f12965e7886710d
RavinduMalshika <ravindu.malshika@gmail.com>
2026-08-15 09:26:06 +1200

    Added the task results

--- Week3_Activity1/iris_data_file_processing.py
+++ Week3_Activity1/iris_data_file_processing.py
@@ -6,9 +6,15 @@ iris = fetch_ucirepo(id=53)
 # data (as pandas dataframes) 
 X = iris.data.features 
 y = iris.data.targets 
-  
-# metadata 
-print(iris.metadata) 
-  
-# variable information 
-print(iris.variables) 
+
+#total records
+total_records = len(X)
+print(f"Total number of records: {total_records}")
+
+# Number of unique flower species
+total_num_of_flowers = y['class'].nunique()
+print(f"Total number of different flowers: {total_num_of_flowers}")
+
+# Unique species names
+names_of_flowers = y['class'].unique()
+print(f"Flower species: {list(names_of_flowers)}")
