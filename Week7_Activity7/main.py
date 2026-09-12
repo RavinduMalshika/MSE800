176cade412531a7b28b97d338ca60bf3bbb058f3
RavinduMalshika <ravindu.malshika@gmail.com>
2026-09-13 11:10:54 +1200

    Added courses to students

--- Week7_Activity7/main.py
+++ Week7_Activity7/main.py
@@ -54,14 +54,16 @@ class TA(AcademicStaff):
         print(f"Class: {self.class_id}")
 
 class Student(Person):
-    def __init__(self, name, student_id):
+    def __init__(self, name, student_id, courses):
         super().__init__(name)
         self.student_id = student_id
+        self.courses = courses
 
     def display_details(self):
         print("\nStudent Details")
         super().display_details()
         print(f"Student ID: {self.student_id}")
+        print(f"Courses: {self.courses}")
 
 def main():
     lecturer = Lecturer("John", "EMP01", "Python", ["pub1", "pub2"])
@@ -73,7 +75,7 @@ def main():
     nonAcademic = NonAcademicStaff("Jane", "EMP03", "Janitor")
     nonAcademic.display_details()
 
-    student = Student("Will", "ST01")
+    student = Student("Will", "ST01", ["Python"])
     student.display_details()
 
 if __name__ == "__main__":
